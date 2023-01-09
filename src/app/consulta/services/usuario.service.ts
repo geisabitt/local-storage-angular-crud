48cca1c64a7dc8c55f648bc00dd4c3ce35fa4801
Geisabitt <ageisislane@gmail.com>
2023-01-09 17:02:15 -0300

    atualizando a forma de salvar os arquivos

--- src/app/consulta/services/usuario.service.ts
+++ src/app/consulta/services/usuario.service.ts
@@ -8,8 +8,50 @@ import { Usuario } from '../model/usuario';
 export class UsuarioService {
   constructor() {}
   salvar(usuario: Usuario) {
-    localStorage.setItem(usuario.cpf.toString(), JSON.stringify(usuario));
+    const data = {
+      dados_pessoais: {
+        nome: usuario.nome,
+        sobrenome: usuario.sobrenome,
+        data_nascimento: usuario.data_nascimento,
+        cpf: usuario.cpf,
+      },
+      endereco: {
+        cep: usuario.cep,
+        endereco: usuario.endereco,
+        numero: usuario.numero,
+        complemento: usuario.complemento,
+        cidade: usuario.cidade,
+        estado: usuario.estado,
+      },
+    };
+    localStorage.setItem(usuario.cpf.toString(), JSON.stringify(data));
   }
+
+  /*estruturardado(usuarioEstruturado: Usuario){
+     const data ={
+      dados_pessoais:{
+        nome: usuarioEstruturado.nome,
+        sobrenome: usuarioEstruturado.sobrenome,
+        data_nascimento: usuarioEstruturado.data_nascimento,
+        cpf: usuarioEstruturado.cpf
+      },
+      endereco:{
+        cep: usuarioEstruturado.cep,
+        endereco: usuarioEstruturado.endereco,
+        numero: usuarioEstruturado.numero,
+        complemento: usuarioEstruturado.complemento,
+        cidade: usuarioEstruturado.cidade,
+        estado: usuarioEstruturado.estado
+      }
+     }
+     return data
+  }
+
+  objNovoEstruturado = .this.estruturardado(: Array<any>): Array<any> {}
+enviarParaOLocalStorage(objNovoEstruturado){
+
+}*/
+
   consultar(): Array<Usuario> {
     let usuarios: Usuario[] = [];
     console.log(localStorage.length);
