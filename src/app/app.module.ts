a499c05295c394f3f0d2c278df3b3d3d78a443ca
Geisabitt <ageisislane@gmail.com>
2023-01-16 08:25:16 -0300

    sistema de mensagens do sistema

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
--- src/app/consulta/cadastro-form/cadastro-form.component.ts
+++ src/app/consulta/cadastro-form/cadastro-form.component.ts
@@ -20,10 +20,10 @@ export class CadastroFormComponent implements OnInit {
     private messagesService: MessagesService
   ) {
     this.formulario = this.formBuilder.group({
-      nome: ['', [Validators.required, Validators.minLength(3)]],
+      nome: [null, [Validators.required, Validators.minLength(3)]],
       sobrenome: ['', [Validators.required, Validators.minLength(3)]],
       cpf: ['', [Validators.required, Validators.maxLength(11)]],
-      data_nascimento: ['', [Validators.required]],
+      data_nascimento: ['', Validators.required],
       cep: ['', [Validators.required, Validators.maxLength(8)]],
       endereco: ['', [Validators.required]],
       numero: ['', [Validators.required]],
@@ -32,52 +32,13 @@ export class CadastroFormComponent implements OnInit {
       estado: ['', [Validators.required, Validators.maxLength(2)]],
     });
   }
-  get nome() {
-    return this.formulario.get('nome')!;
-  }
-  get sobrenome() {
-    return this.formulario.get('sobrenome')!;
-  }
-  get cpf() {
-    return this.formulario.get('cpf')!;
-  }
-  get data_nascimento() {
-    return this.formulario.get('data_nascimento')!;
-  }
-  get cep() {
-    return this.formulario.get('cep')!;
-  }
-  get endereco() {
-    return this.formulario.get('endereco')!;
-  }
-  get numero() {
-    return this.formulario.get('numero')!;
-  }
-  get complemento() {
-    return this.formulario.get('complemento')!;
-  }
-  get cidade() {
-    return this.formulario.get('cidade')!;
-  }
-  get estado() {
-    return this.formulario.get('estado')!;
-  }
 
-  //data!: Usuario;
-
-  ngOnInit(): void {
-    //this.data = new Usuario();
-  }
-  /*salvar(form: NgForm) {
-    if (form.valid) {
-      this.usuarioService.salvar(this.data);
-    }
-  }*/
+  ngOnInit(): void {}
 
   onSubmit() {
     if (this.formulario.invalid) {
       this.messagesService.add(
-        'Confira se preencheu todos os dados de forma correta.'
+        `Confira se preencheu todos os dados de forma correta. -- Os campos marcados de vermelho não estão preenchidos corretamente.`
       );
       return;
     } else {
